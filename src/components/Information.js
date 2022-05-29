afab2f4365b49de60d8f791e77738c4f386d9c23
JordanMahaut <jordanmahaut09@gmail.com>
2022-05-29 08:32:43 +0200

    Fusion

---
+++ src/components/Information.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Information = () => {
+  return (
+    <div className="contact">
+      <p>
+        <strong>Email : </strong> <a href="jordanmahaut09@gmail.com"></a>
+      </p>
+      <p>
+        <strong>Numéro de téléphone : </strong>06 02 40 58 14
+      </p>
+      <p>
+        <strong>LinkedIn : </strong>{" "}
+        <a href="https://www.linkedin.com/in/jordan-mahaut-b30908227/">
+          Jordan Mahaut
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default Information;
